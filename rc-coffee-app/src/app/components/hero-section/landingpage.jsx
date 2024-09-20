0bb7012ca4c295226f4ea20dd91c5e2589fafbb5
cortezrc611 <cortezrolly611@gmail.com>
2024-09-21 00:11:49 +0800

    first commit

---
+++ rc-coffee-app/src/app/components/hero-section/landingpage.jsx
@@ -0,0 +1,18 @@
+export default function LandingPage() {
+    return (
+      <main>
+        <div className="beans relative w-full h-screen mt-20 overflow-hidden">
+          <div className="background-image absolute inset-0 bg-cover bg-no-repeat bg-center z-0"></div>
+          <div className="coffee-slogan flex items-center flex-row justify-center h-full z-10 text-white italic font-bold text-4xl text-center">
+            <div className="slogan">
+              <h3>FEEL TIRED?</h3>
+              <h1>RELAX... TAKE A</h1>
+              <h1>COFFEE BREAK</h1>
+            </div>
+            <img src="./images/hero-image.png" alt="Hero" className="mt-4" />
+          </div>
+        </div>
+      </main>
+    );
+  }
+  
---
+++ rc-coffee-app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "./components/navbar/Navbar";
+import LandingPage from "./components/hero-section/landingpage";
+export default function Home() {
+  return (
+    <>
+   <Navbar />
+   <LandingPage />
+   </>
+  );
+}
