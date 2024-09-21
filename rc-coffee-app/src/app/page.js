3f7268370d2a863ad461b1ae2965861dede55a90
cortezrc611 <cortezrolly611@gmail.com>
2024-09-21 16:24:31 +0800

    about done

--- rc-coffee-app/src/app/page.js
+++ rc-coffee-app/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./components/hero-section/landingpage";
+import About from "./components/about/about"
 export default function Home() {
   return (
     <>
    <Navbar />
    <LandingPage />
+   <About />
    </>
   );
 }
