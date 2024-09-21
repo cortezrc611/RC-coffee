1b7c539460b1887198e3c7d6e7e57e7926c8bb2f
cortezrc611 <cortezrolly611@gmail.com>
2024-09-21 16:49:42 +0800

    add readmore

--- rc-coffee-app/src/app/components/about/about.jsx
+++ rc-coffee-app/src/app/components/about/about.jsx
@@ -6,6 +6,7 @@ export default function About(){
     <div className="about-text font-sans italic">
    <h1 className="font-bold text-center text-[#F6E7C7]">ABOUT US</h1>
    <p className=" font-light text-[#F6E7C7]">Welcome to RC Cafe, your cozy neighborhood coffee shop dedicated to crafting the perfect cup. We source our beans from sustainable farms around the world, ensuring every sip is rich, fresh, and full of flavor. Whether you're here for a quick pick-me-up or to relax with friends, our inviting space and passionate baristas are ready to make your coffee experience unforgettable. Come for the coffee, stay for the community!</p>
+   <p className="mt-6 text-center font-extrabold"><a className=" hover:text-[#F6E7C7] transition-all" href="/">Read More...</a></p>
     </div>
     <img src="./images/aboutimage.jpg" alt="about" />
    </div>
